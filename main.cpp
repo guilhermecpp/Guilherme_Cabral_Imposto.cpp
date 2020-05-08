9c7f95d6fd1db9d4094e4fe6e93abbe12ded9a96
guilhermecpp <guilhermecdemenezes123@gmail.com>
2020-05-07 22:08:39 -0300

    Leitura de dados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    double salario;
+    double prestacao_de_servico;
+    double ganho_de_capital;
+    double gastos_medicos;
+    double gastos_educacionais;
+
+    printf("Renda anual com salario: ");
+    scanf("%lf", &salario);
+
+    printf("Renda anual com prestacao de servico: ");
+    scanf("%lf", &prestacao_de_servico);
+
+    printf("Renda anual com ganho de capital: ");
+    scanf("%lf", &ganho_de_capital);
+
+    printf("Gastos medicos: ");
+    scanf("%lf", &gastos_medicos);
+
+    printf("Gastos educacionais: ");
+    scanf("%lf", &gastos_educacionais);
+
     return 0;
 }
